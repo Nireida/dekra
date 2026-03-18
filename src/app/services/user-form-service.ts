2a8882cb04bf953403e99f17d660b31cf95b58cc
ngyulamirova2 <388771@gmail.com>
2026-03-18 01:39:05 +0100

    add validation

--- src/app/services/user-form-service.ts
+++ src/app/services/user-form-service.ts
@@ -7,10 +7,14 @@ import {User} from '../interfaces/user.model';
 export class UserFormService {
   getQuestions(): QuestionBase<User>[] {
     return [
-      { key: 'username', label: 'Username', controlType: controlType.textbox, required: true, order: 1 },
-      { key: 'name', label: 'First Name', controlType: controlType.textbox, required: true, order: 2 },
-      { key: 'surnames', label: 'Surnames', controlType: controlType.textbox, required: true, order: 3 },
-      { key: 'email', label: 'Email', type: 'email', controlType: controlType.textbox, required: true, order: 4 },
+      { key: 'username', label: 'Username', controlType: controlType.textbox, required: true, order: 1, errors: {
+          required: 'Username is required'} },
+      { key: 'name', label: 'First Name', controlType: controlType.textbox, required: true, order: 2, errors: {
+          required: 'First Name is required'} },
+      { key: 'surnames', label: 'Surnames', controlType: controlType.textbox, required: true, order: 3, errors: {
+          required: 'Surnames is required'}},
+      { key: 'email', label: 'Email', type: 'email', controlType: controlType.textbox, required: true, order: 4, errors: {
+          required: 'Email is required'}},
       { key: 'password', label: 'Password', type: 'password', controlType: controlType.textbox, required: true, order: 5, errors: {
           required: 'Password is required',
           minlength: 'Minimum 8 characters',
@@ -18,7 +22,8 @@ export class UserFormService {
         } },
       { key: 'age', label: 'Age', type: 'number', controlType: controlType.textbox, required: true, order: 6, errors: {
           min: 'Minimum age is 0',
-          max: 'Maximum age is 120'
+          max: 'Maximum age is 120',
+          required: 'Age is required',
         } },
       { key: 'active', label: 'Active Status', controlType: controlType.checkbox, order: 7, value: false }
     ].sort((a, b) => a.order - b.order) as QuestionBase<User>[];
